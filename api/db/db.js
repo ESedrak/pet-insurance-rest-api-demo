e919e012b3235fc42d9a09bdee4bb6a317437217
ESedrak <elizabeth_sedrak@hotmail.com>
2022-08-28 20:18:59 +1000

    add find dbconnection collections for fun

--- api/db/db.js
+++ api/db/db.js
@@ -1,37 +1,53 @@
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost/mydb";
 
 let dbConnection;
 
 module.exports = {
-    connectToServer: function (callback) {
-        MongoClient.connect(url, function(err, db) {
-            if (err || !db) {
-                return callback(err);
-            }
-            console.log("Database created!");
+	connectToServer: function (callback) {
+		MongoClient.connect(url, function (err, db) {
+			if (err || !db) {
+				return callback(err);
+			}
+			console.log("Database created!");
 
-            dbConnection = db.db("mydb");
-            console.log('Successfully connected to MongoDB.');
+			dbConnection = db.db("mydb");
+			console.log("Successfully connected to MongoDB.");
 
-            // clear db from previous run (just for demo purposes)
-            // mongodb local server persists records and we want a clean slate each time we start our API server
-            dbConnection.collection("pets").deleteMany( {} )
+			// clear db from previous run (just for demo purposes)
+			// mongodb local server persists records and we want a clean slate each time we start our API server
+			dbConnection.collection("pets").deleteMany({});
 
-            let pets = require('./pets.json');
+			let pets = require("./pets.json");
 
-            dbConnection.collection("pets").insertMany(pets, function(err) {
-                if (err) {
-                    return callback(err);
-                  }
-                console.log("1 document inserted");
-            });
+			dbConnection.collection("pets").insertMany(pets, function (err, res) {
+				if (err) {
+					return callback(err);
+				}
+				console.log("1 document inserted");
+			});
 
-            return callback();
-        });
-    },
+			dbConnection
+				.collection("pets")
+				.find({})
+				.toArray(function (err, result) {
+					if (err) throw err;
+					console.log(result);
+				});
 
-    getDb: function () {
-        return dbConnection;
-    },
+			dbConnection
+				.collection("pets")
+				.find({ type: "dog" })
+				.toArray(function (err, result) {
+					if (err) throw err;
+					console.log(result);
+				});
+
+			return callback();
+		});
+	},
+
+	getDb: function () {
+		return dbConnection;
+	},
 };
