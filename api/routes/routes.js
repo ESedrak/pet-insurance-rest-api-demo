6d59f6084b46ab3a5cc4356c0502e7888bd86fb5
ESedrak <elizabeth_sedrak@hotmail.com>
2022-08-28 20:19:35 +1000

    endpoint for pets done (all data)

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,11 +1,12 @@
-const
-  express = require('express');
-  bodyParser = require('body-parser')
+const express = require("express");
+bodyParser = require("body-parser");
 
 // create application/json parser - used for /post method only
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
 app.use(bodyParser.json());
 
 // routes is an instance of the express router
@@ -14,19 +15,35 @@ app.use(bodyParser.json());
 const routes = express.Router();
 
 // This will help us connect to the database
-const dbo = require('../db/db');
+const dbo = require("../db/db");
+
+routes.route("/pets").get(async function (_req, res) {
+	let pets = [];
+	dbo
+		.getDb()
+		.collection("pets")
+		.find()
+		.sort({ risk: 1 })
+		.forEach((pet) => pets.push(pet))
+		.then(() => {
+			res.status(200).json(pets);
+		})
+		.catch(() => {
+			res.status(500).json({ error: "Something's wrong" });
+		});
+});
 
 // Health check endpoint
 // Endpoint to call to ensure your API is up and healthy
 // Can use a service to call this and report the health of your API using CI
-routes.route('/health').get(async function(_req, res) {
-  const data = {
-    uptime: process.uptime(),
-    message: 'OK',
-    date: new Date()
-  }
+routes.route("/health").get(async function (_req, res) {
+	const data = {
+		uptime: process.uptime(),
+		message: "OK",
+		date: new Date(),
+	};
 
-  res.status(200).send(data);
+	res.status(200).send(data);
 });
 
 module.exports = routes;
